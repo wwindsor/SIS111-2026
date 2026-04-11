324352e3b4f654eac9cdf88711d745f3df6d8b95
Win Alvarez <windsoralvarezdavila@gmail.com>
2026-04-11 09:31:55 -0400

    feat: bubble sort en python y typescript

---
+++ EJERCICIOS/1.py
@@ -0,0 +1,8 @@
+# Crea un programa que en un rango de numeros del 1 al 100 aleatorios 
+# Cantidad n numeros aleatorios para generar dos listas una de numeros pares 
+# y otro de impares, deberá mostrar el total de la suma de la lista de pares 
+# y la de impares
+
+[2,4,6] el total es 12
+
+[1,3,5] el total es 9
---
+++ TEMA3/python/bubblesort.py
@@ -0,0 +1,19 @@
+def bubblesort(lista, ascendente=True):
+    cantidad = len(lista)
+    for i in range(cantidad-1):
+        for j in range(cantidad-i-1):
+            #aux = 0
+                if (ascendente and lista[j] > lista[j + 1]) or \
+                (not ascendente and lista[j] < lista[j + 1]):
+                #Sin manejo de auxiliares y manejo de respectivos#
+                    lista[j],lista[j+1] = lista[j+1],lista[j]
+                #Con manejo de auxiliares#
+                    # aux = lista[j]
+                    # lista[j] = lista[j+1]
+                    # lista[j+1] = aux
+    return lista
+
+list = [7, 12, 9, 11, 3]
+
+print(bubblesort(list, True))
+print(bubblesort(list,False))
