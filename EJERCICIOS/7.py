587ac1b74fc9cdffe776d6c979a04e2818de9386
Win Alvarez <windsoralvarezdavila@gmail.com>
2026-04-30 17:45:35 -0400

    feat: ejercicio 7 productos

---
+++ EJERCICIOS/7.py
@@ -0,0 +1,22 @@
+"""
+Una persona que va de compras a la multitienda "CyberSTORE" decide llevar 
+un control sobre lo que va comprando, para saber la cantidad de dinero 
+que tendrá que pagar al llegar a la caja
+La tienda solamente vende 3 tipos de productos, estos pueden ser 
+Televisores (codigo 1), Refrigeradores (codigo 2), Lavadoras (codigo 3)
+No se puede ingresar el codigo de otro articulo, por lo que solo esos 3
+valores pueden ser ingresados (validacion)
+La persona comprará exactamente 3 productos...
+
+Ud. debe realizar un Algoritmo que permita, en cada uno de los 3 articulos:
+    > Indicar el codigo
+    > Ingresar la cantidad
+    > Ingresar el valor
+
+Finalmente, el algoritmo debe dar como respuesta lo siguiente:
+
+Se compraron X televisores, a un total de XXXXX
+Se compraron Y refrigeradores, a un total de XXXXX
+Se compraron Z lavadoras, a un total de XXXXX
+
+"""
