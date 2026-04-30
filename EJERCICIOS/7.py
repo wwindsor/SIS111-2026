097c88cc826ddae40b323961f04bdfdb5d68f5a0
Win Alvarez <windsoralvarezdavila@gmail.com>
2026-04-30 18:41:06 -0400

    feat: ejercicio 7 completo

--- EJERCICIOS/7.py
+++ EJERCICIOS/7.py
@@ -6,12 +6,12 @@ La tienda solamente vende 3 tipos de productos, estos pueden ser
 Televisores (codigo 1), Refrigeradores (codigo 2), Lavadoras (codigo 3)
 No se puede ingresar el codigo de otro articulo, por lo que solo esos 3
 valores pueden ser ingresados (validacion)
-La persona ira exactamente 3 veces a la tienda...
+La persona tiene que ir 3 veces a la tienda...
 
 Ud. debe realizar un Algoritmo que permita, en cada vez que visite la tienda se:
     > Indicar el codigo
     > Ingresar la cantidad
-    > Ingresar el valor
+    > Ingresar el precio unitario
 
 Finalmente, el algoritmo debe dar como respuesta lo siguiente:
 
@@ -27,9 +27,10 @@ def validar(x):
     else: return False
 
 def ingresarDatos():
-    print('Seleccione su producto')
+    print('Seleccione su producto...')
     print('Televisores (codigo 1), Refrigeradores (codigo 2), Lavadoras (codigo 3)')
-    while(range(3)<3):
+    productos = []
+    for _ in range(3):
         print(f"Ingresar codigo")
         codigo = int(input())
         if(validar(codigo)):
@@ -37,17 +38,31 @@ def ingresarDatos():
             cantidad = int(input())
             print(f"Ingresar el valor")
             valor = float(input())
-        else: ingresarDatos(); break; 
+        else: print("error") 
+        producto = {"codigo": codigo, "cantidad":cantidad,"valor":valor}
+        productos.append(producto)
+    return productos
 
-    asignaturas=("Programacion","Algebra Lineal","Ing de Sistemas")
-    datos = []
-    for asignatura in asignaturas:
-        print(f"Ingrese la nota de examen de {asignatura}")
-        examen = float(input())
-        print(f"Ingrese la nota de practica 1  de {asignatura}")
-        practica1 = float(input())
-        print(f"Ingrese la nota de practica 2 de {asignatura}")
-        practica2 = float(input())
-        datos.append({"asignatura":asignatura,"examen":examen,
-                      "practica1":practica1,"practica2":practica2})
-    return datos
+def calculo1():
+    productos = ingresarDatos() 
+    cantidad1=0
+    cantidad2=0
+    cantidad3=0
+    valor1=0
+    valor2=0
+    valor3=0
+    for producto in productos:
+        if(producto["codigo"]==1):
+            cantidad1+=producto["cantidad"]
+            valor1+=(producto["valor"]*producto["cantidad"])
+        if(producto["codigo"]==2):
+            cantidad2+=producto["cantidad"]
+            valor2+=(producto["valor"]*producto["cantidad"]) 
+        if(producto["codigo"]==3):
+            cantidad3+=producto["cantidad"]
+            valor3+=(producto["valor"]*producto["cantidad"])
+    print(f"Se compraron {cantidad1} televisores, a un total de {valor1}")
+    print(f"Se compraron {cantidad2} refrigeradores, a un total de {valor2}")
+    print(f"Se compraron {cantidad3} lavadoras, a un total de {valor3}")
+
+calculo1()
